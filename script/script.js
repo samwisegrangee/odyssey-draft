c98937f2c31320942ff684e4203d1b2e18855b47
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-07-10 15:58:05 -0400

    Toggle Lang
    Added toggle button between Greek-English verses.
    
    Still need to add links to Greek, so they can toggle English visibility.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-
 $(document).ready(function(){
+
   //Smooth scrolling between ""#anchor" links.
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
@@ -13,9 +13,22 @@ $(document).ready(function(){
 	        window.location.hash = target;
 	    });
 	});
+
 });
 
-  //Toggle visibility for Greek verses
+//Toggle visibility for Greek verses by Id
+function toggleDiv(divId) {
+	$("#"+divId).toggle(1000);
+};
+
+$('.lang-btn').click(function(){
+	$('.gr-v').toggle(1000);
+	$('.eng-v').toggle(1000);
+});
+
+
+
+  //Toggle visibility for Greek verses by Class
   //http://jsfiddle.net/krY56/1/ toggle display:none
 
   //$(document).ready(function(){
@@ -30,6 +43,11 @@ $(document).ready(function(){
 
 //});
 
-  function toggleDiv(divId) {
-    $("#"+divId).toggle(1000);
-  }
+	//Give a parallax effect on the .hero, but the stacking order needs work to hide the edges.
+	//$(window).scroll(function(){
+		//var wScroll = $(this).scrollTop();
+
+		//$('.hero').css({
+			//'transform' : 'translate(0px, '+ wScroll /3 +'%)'
+		//});
+	//});
