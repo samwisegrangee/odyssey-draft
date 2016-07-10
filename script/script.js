e47ac07bc126dd34bc4a53bde1cc087bf5a07450
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-07-09 22:32:07 -0400

    Verses
    Added 20 more verses to Bk 1 and the first 4 verses of Bk 2.
    
    Centered .kicker
    
    Disabled Books 3-22

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function(){
+  //Smooth scrolling between ""#anchor" links.
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
@@ -13,6 +15,21 @@ $(document).ready(function(){
 	});
 });
 
-function toggleDiv(divId) {
-  $("#"+divId).toggle(1000);
-}
+  //Toggle visibility for Greek verses
+  //http://jsfiddle.net/krY56/1/ toggle display:none
+
+  //$(document).ready(function(){
+
+//$("gr-v").hide();
+
+  //$("gr-en").click(function(){
+
+    //$("gr-v").toggle();
+
+  //});
+
+//});
+
+  function toggleDiv(divId) {
+    $("#"+divId).toggle(1000);
+  }
