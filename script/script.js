62264678e58ed090b559f68a22edc15652050426
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-07-11 12:56:23 -0400

    Script cleaning

--- script/script.js
+++ script/script.js
@@ -15,33 +15,28 @@ $(document).ready(function(){
 	});
 
 });
-//Toggle visibility for Greek verses by Id
-function toggleDiv(divId) {
-	$("#"+divId).toggle(1000);
-};
-//Fade out all English verses then fadeIn Greek verses (by class).
-$('.gr-btn').click(function(){
-	$('.eng-v').fadeOut(500, function(){
-		$('.gr-v').fadeIn(1000);
+	//Toggle visibility for Greek verses by Id
+	function toggleDiv(divId) {
+		$("#"+divId).toggle(1000);
+	};
+	//Fade out all English verses then fadeIn Greek verses (by class).
+	$('.gr-btn').click(function(){
+		$('.eng-v').fadeOut(500, function(){
+			$('.gr-v').fadeIn(1000);
+		});
 	});
-});
-//Fade out all English verses then fadeIn Greek verses (by class).
-$('.eng-btn').click(function(){
-	$('.gr-v').fadeOut(500, function(){
-		$('.eng-v').fadeIn(1000);
+	//Fade out all English verses then fadeIn Greek verses (by class).
+	$('.eng-btn').click(function(){
+		$('.gr-v').fadeOut(500, function(){
+			$('.eng-v').fadeIn(1000);
+		});
 	});
-});
 
+//Give a parallax effect on the .hero, but the stacking order needs work to hide the edges.
+//$(window).scroll(function(){
+	//var wScroll = $(this).scrollTop();
 
-
-
-
-
-	//Give a parallax effect on the .hero, but the stacking order needs work to hide the edges.
-	//$(window).scroll(function(){
-		//var wScroll = $(this).scrollTop();
-
-		//$('.hero').css({
-			//'transform' : 'translate(0px, '+ wScroll /3 +'%)'
-		//});
+	//$('.hero').css({
+		//'transform' : 'translate(0px, '+ wScroll /3 +'%)'
 	//});
+//});
