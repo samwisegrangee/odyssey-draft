f73f1ed3a339f05ad4284745f136736888432d10
samwisegrangee <Samwise@Samuels-MacBook-Pro.local>
2016-07-16 12:23:19 -0400

    K. I. S. S.
    Radio buttons. If statements. JavaScript and jQuery.
    Bind/unbind(‘click, function … etc.
    
    I’ve been overthinking this. Simple solution. Create two versions of
    the book: the Greek and the English. Click on the GR/EN buttons will
    take you to one or the other. Boom. Done.
    
    Now I just need a day to consolidate all these redundant styles I’ve
    been drafting…

--- script/script.js
+++ script/script.js
@@ -21,24 +21,33 @@ $(document).ready(function(){
 		$("#"+divId).toggle(1000);
 	};
 	//Fade out all English verses then fadeIn Greek verses (by class).
-	$('.gr-btn').click(function(){
-		$('.eng-v').fadeOut(500, function(){
-			$('.gr-v').fadeIn(1000, function (){
-				$('.eng-v').bind('click', function(e){ //Disables .eng-v's toggle links
-			    e.preventDefault();
-			  });
-			});
-		});
-	});
+	//$('.gr-btn').click(function(){
+		//$('.eng-v').fadeOut(500, function(){
+			//$('.gr-v').fadeIn(1000, function (){
+				//$('.eng-v').bind('click', function(e){ //Disables .eng-v's toggle links
+			    //e.preventDefault();
+			  //});
+			//});
+		//});
+	//});
 
 	//Fade out all Greek verses then fadeIn English verses (by class).
-	$('.eng-btn').click(function(){
-		$('.gr-v').fadeOut(500, function(){
-			$('.eng-v').fadeIn(1000, function(){
-				$('.eng-v').unbind('click');
-			});
-		});
-	});
+	//$('.eng-btn').click(function(){
+		//$('.gr-v').fadeOut(500, function(){
+			//$('.eng-v').fadeIn(1000, function(){
+			//	$('.eng-v').unbind('click');
+			//});
+		//});
+	//});
+
+	//function DisableLinks(className) {
+  //   $('a.'+ className).click(function () {
+    //      return false;
+     //}); }
+//function EnableLinks(className){
+  //   $('a.'+ className).click(function () {
+    //      return true;
+     //});
 //Give a parallax effect on the .hero, but the stacking order or the border size needs work to hide the edges.
 //$(window).scroll(function(){
 	//var wScroll = $(this).scrollTop();
@@ -47,4 +56,16 @@ $(document).ready(function(){
 	//$('.hero').css({
 		//'transform' : 'translate(0px, '+ wScroll /3 +'%)'
 	//});
+//});
+//$(document).ready(function() {
+
+//  $('#yourdiv').click(function() {
+//   $(this).css('background-color', 'blue');
+//});
+
+//$(document).click(function(e) {
+//   if(!$(e.target).is('#yourdiv'))
+//      $('#yourdiv').css('background-color', 'red');
+//});
+
 //});
