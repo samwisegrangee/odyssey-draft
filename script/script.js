fa193c922e62788b6e5402f3beb51ed704fabb4a
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-07-19 13:51:15 -0400

    Sticky Menu, Copy Drafted
    Now when you scroll from the top of of the page into the About section
    et al., the Menu button will stick to the top and follow you down. Ah,
    seamless…
    
    Also, there’s an About section regarding The Odyssey and about this
    site.
    Finally I added a Contact page and FAQ section.
    
    I’m thinking about doing some parallax scrolling on the wording I have
    overlaying images…

--- script/script.js
+++ script/script.js
@@ -1,6 +1,17 @@
 //Make All BOOK page loads default to an "EN state" ('.eng-btn' pressed)
 
 $(document).ready(function(){
+	// Sticky nav bar
+	var hn = $(".home-nav");
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 780 ) {
+			hn.addClass("home-nav-scrolled");
+		}
+		else {
+			hn.removeClass("home-nav-scrolled")
+		}
+	});
 
   //Smooth scrolling between ""#anchor" links.
 	$('a[href^="#"]').on('click',function (e) {
@@ -20,52 +31,3 @@ $(document).ready(function(){
 	function toggleDiv(divId) {
 		$("#"+divId).toggle(1000);
 	};
-	//Fade out all English verses then fadeIn Greek verses (by class).
-	//$('.gr-btn').click(function(){
-		//$('.eng-v').fadeOut(500, function(){
-			//$('.gr-v').fadeIn(1000, function (){
-				//$('.eng-v').bind('click', function(e){ //Disables .eng-v's toggle links
-			    //e.preventDefault();
-			  //});
-			//});
-		//});
-	//});
-
-	//Fade out all Greek verses then fadeIn English verses (by class).
-	//$('.eng-btn').click(function(){
-		//$('.gr-v').fadeOut(500, function(){
-			//$('.eng-v').fadeIn(1000, function(){
-			//	$('.eng-v').unbind('click');
-			//});
-		//});
-	//});
-
-	//function DisableLinks(className) {
-  //   $('a.'+ className).click(function () {
-    //      return false;
-     //}); }
-//function EnableLinks(className){
-  //   $('a.'+ className).click(function () {
-    //      return true;
-     //});
-//Give a parallax effect on the .hero, but the stacking order or the border size needs work to hide the edges.
-//$(window).scroll(function(){
-	//var wScroll = $(this).scrollTop();
-
-
-	//$('.hero').css({
-		//'transform' : 'translate(0px, '+ wScroll /3 +'%)'
-	//});
-//});
-//$(document).ready(function() {
-
-//  $('#yourdiv').click(function() {
-//   $(this).css('background-color', 'blue');
-//});
-
-//$(document).click(function(e) {
-//   if(!$(e.target).is('#yourdiv'))
-//      $('#yourdiv').css('background-color', 'red');
-//});
-
-//});
